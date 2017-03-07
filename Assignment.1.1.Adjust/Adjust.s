c7734e8ca7a3f9eb2dc52dfe5baead7b7a845785
Jorik Schellekens <JorikSch@gmail.com>
2017-03-07 11:19:58 +0000

    Transversing pixel method written.

--- Assignment.1.1.Adjust/Adjust.s
+++ Assignment.1.1.Adjust/Adjust.s
@@ -1,4 +1,5 @@
 	AREA	Adjust, CODE, READONLY
+	PRESERVE8
 	IMPORT	main
 	IMPORT	getPicAddr
 	IMPORT	putPic
@@ -7,8 +8,8 @@
 	EXPORT	start
 
 getPixelError
-	LDR R0, #-1
-	LDR R1, #-1
+	LDR R0, =-1
+	LDR R1, =-1
 	B getPixelFinally
 getPixel			; address, RGBval = getPixel(row, col)
 	; Parameters:
@@ -27,7 +28,7 @@ getPixel			; address, RGBval = getPixel(row, col)
 	LDR R0, [R4, R0, LSL #2]; RGBvalue = Memmory.word(pictureaddress + addressOffset * 4)
 getPixelFinally
 	LDMFD SP!, {LR}
-	BX
+	BX LR
 	
 rowColToIndex
 	; converts row and colum to index
@@ -38,7 +39,7 @@ rowColToIndex
 	; R0 addressIndex
 	MUL R0, R1, R0		; addressOffset = row * col
 	ADD R0, R0, R1		; addressOffset += col
-	BX
+	BX LR
 
 putPixel
 	; Stores a given RGB to an pixel of at row, col
@@ -52,7 +53,7 @@ putPixel
 	STR R2, [R4, R0, LSL #2]; Memory.word(pictureAddress + addressOffset * 4)
 	
 	LDMFD SP!, {LR}
-	BX
+	BX LR
 	
 adjustPixel
 	; adjustedVal = adjustPixel(value, contrast, brightness)
@@ -79,7 +80,7 @@ adjustPixel
 	B setValueFromMask
 
 	LDMFD SP!, {R2, R3, LR}
-	BX
+	BX LR
 	
 adjustColor 
 	; val = adjustColor(color, contrast, brightness)
@@ -90,16 +91,16 @@ adjustColor
 	; Return Values
 	; R2 = color
 	; Stack > contrast, brightness that order.
-	STMFD SP! {R4, R5}
+	STMFD SP!, {R4, R5}
 	LDR R4, [SP, #-12]	; contrast = stack.getParameter()
 	LDR R5, [SP, #-8]	; brightness = stack.getParameter()
 	MUL R2, R4, R2		; color *= contrast
 	LSR R2, R2, #4		; color /= 16
 	ADDS R2, R2, R5		; color += brightness
-	LDRMI R2, #0		; if (color < 0): color = 0
+	LDRMI R2, =0		; if (color < 0): color = 0
 	CMP R2, #255		; else if (color > 255):
-	LDRGT R2, #255		;	color = 255
-	LDMFD SP! {R4, R5}	; restore pointers
+	LDRGT R2, =255		;	color = 255
+	LDMFD SP!, {R4, R5}	; restore pointers
 	BX LR
 	
 	
@@ -115,31 +116,13 @@ getValueFromMask
 	; Return Values
 	; R2 = colorValue
 	BIC R2, R0, R1		; value = RGB & mask
-while	
+getMaskWhile	
 	LSRS R1, R1, #4		; while (mask >> 4 doesn't carry)
-	BCS endWhile		; {
+	BCS endGetMaskWhile	; {
 	LSR R2, R2, #4		;	value >> 4
-endWhile			; }
+	B getMaskWhile		; }
+endGetMaskWhile			
 	BX LR
-
-
-getValueFromMask
-	; Gets the color value under a congruent mask
-	; Expects masks of type FF
-	; eg mask 00FF0000 will return the value under FF in this case the value of red
-	; Parameters
-	; R0 = RGB
-	; R1 = mask
-	; Return Values
-	; R2 = colorValue
-	BIC R2, R0, R1		; value = RGB & mask
-while	
-	LSRS R1, R1, #4		; while (mask >> 4 doesn't carry)
-	BCS endWhile		; {
-	LSR R2, R2, #4		;	value >> 4
-endWhile			; }
-	BX LR
-	
 	
 setValueFromMask
 	; Sets the color value under a congruent mask
@@ -153,11 +136,12 @@ setValueFromMask
 	; R0 = RGB
 	MVN R1, R1		; invertmask for simplicity
 	BIC R0, R0, R1		; RGB = RGB & mask // remove color
-while	
+setMaskWhile	
 	LSRS R1, R1, #4		; while (mask >> 4 doesn't carry)
-	BCC endWhile		; {
+	BCC endSetMaskWhile		; {
 	LSL R2, R2, #4		;	value >> 4
-endWhile			; }
+	B setMaskWhile		; }
+endSetMaskWhile			;
 	ADD R0, R0, R2		; RGB = RGB + value
 	BX LR
 	
@@ -168,10 +152,31 @@ start
 	MOV	R5, R0
 	BL	getPicWidth	; load the width of the image (columns) in R6
 	MOV	R6, R0
+	
+	LDR R8, =20	; 	Contrast
+	LDR R9, = 0	;	Brightness
 
-
-
-	BL putPic		
+iLoop
+	MOV R7, R6
+	SUBS R5, R5, #1
+	BMI endiLoop
+jLoop
+	SUBS R7, R7, #1
+	BMI endjLoop
+	MOV R0, R5
+	MOV R1, R7
+	BL getPixel
+	MOV R2, R8
+	MOV R3, R9
+	BL adjustPixel
+	MOV R0, R5
+	MOV R1, R7
+	BL putPixel
+	B jLoop
+endjLoop
+	B iLoop
+endiLoop
+	BL putPic
 	; re-display the updated image
 
 stop	B	stop
